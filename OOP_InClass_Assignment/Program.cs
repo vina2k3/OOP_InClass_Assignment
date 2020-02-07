0af030afb632d78aa5e56986b1c5a0b35d73ca41
Vinicius de Souza <vsouza@usf.edu>
2020-02-07 12:22:57 -0500

    Add project files.

---
+++ OOP_InClass_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP_InClass_Assignment
+{
+    class Car
+    {
+        public string color = "red";
+        static void Main(string[] args)
+        {
+            Car myObj1 = new Car();
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj2.color);
+        }
+    }
+    class MyClass
+    {
+        string color = "red";
+        int maxSpeed = 200;
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+
+}
